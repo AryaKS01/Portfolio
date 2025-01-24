c8961000c0af5e48266ab1ed5f8ea286f14fd0f0
AryaKS01 <aryaworks24@gmail.com>
2025-01-24 17:30:11 +0530

    update assets

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,43 +1,42 @@
-import Project1 from "../../assets/project1.webp";
-import Project2 from "../../assets/project2.webp";
-import Project3 from "../../assets/project3.webp";
-
+import Project1 from "../../assets/journey.png";
+import Project2 from "../../assets/casio.png";
+import Project3 from "../../assets/hotel.png";
 
 export const projectsData = [
-    {
-        id: 1,
-        image: Project1,
-        title: "Figma design of a watch e-commerce",
-        category: "A",
-        link:"https://www.figma.com/design/jNPvrrQ5SB4PS5yuAS7Ira/Ecommerce-Website?node-id=0-1&t=pNgQPoDSYbrZqskk-1"
-    },
-    {
-        id: 2,
-        image: Project2,
-        title: "Real-esate website UI",
-        category: "B",
-        link:"https://real-estate-project-five.vercel.app/"
-    },
-    {
-        id: 3,
-        image: Project3,
-        title: "Crypto value tracker",
-        category: "C",
-        link:"https://crypto-exchange-live-value.vercel.app/"
-    },
+  {
+    id: 1,
+    image: Project1,
+    title: "Journey Nest",
+    category: "A",
+    link: "https://journeynest.onrender.com/listings",
+  },
+  {
+    id: 2,
+    image: Project2,
+    title: "Casio Website Clone",
+    category: "B",
+    link: "https://real-estate-project-five.vercel.app/",
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: "Hotel Rooftop Blog",
+    category: "C",
+    link: "https://hotels-rooftop-blog-frontend.vercel.app/",
+  },
 ];
 
 export const projectsNav = [
-    {
-        name: "All",
-    },
-    {
-        name: "A",
-    },
-    {
-        name: "B",
-    },
-    {
-        name: "C",
-    },
+  {
+    name: "All",
+  },
+  {
+    name: "A",
+  },
+  {
+    name: "B",
+  },
+  {
+    name: "C",
+  },
 ];
